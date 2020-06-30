d07c6e5e1272ca5ce7f97ca6a57277e0bc452443
“hualli” <robertotorfe@gmail.com>
2020-06-30 15:30:39 -0300

    creo las migraciones de pacientes, obras sociales y consultas - creo el diagrama de db

---
+++ database/migrations/2020_06_30_181805_create_consultas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsultasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consultas', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id');
+            $table->dateTime('fecha_nacimiento')->nullable();
+            $table->text('diagnostico');
+            $table->text('tratamiento');
+            $table->bigInteger('paciente_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('paciente_id')
+                  ->references('id')
+                  ->on('pacientes')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consultas');
+    }
+}
