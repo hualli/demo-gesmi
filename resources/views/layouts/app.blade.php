5a17511a182763b5f40c525bebaefe9c2b304375
hualli <robertotorfe@gmail.com>
2020-10-19 18:16:38 -0300

    creo index de usuarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -180,7 +180,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('usuarios.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Usuarios</p>
                 </a>
